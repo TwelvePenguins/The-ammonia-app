c209e9703e5c280d5953716b490c06d26c2fe4c0
Du Yuhan <yuhan.du@gmail.com>
2025-07-17 22:59:13 +0800

    Add dynamically updated upcoming meals

--- The ammonia app/HomeView.swift
+++ The ammonia app/HomeView.swift
@@ -41,120 +41,131 @@ struct HomeView: View {
                     .font(.callout)
                     .padding(.leading, 20)
                 TabView {
-                    ForEach($upcomingMeals, id: \.self) { $meal in // Add case for no meal
-                        VStack(alignment: .leading) {
+                    if !upcomingMeals.isEmpty {
+                        ForEach($upcomingMeals, id: \.self) { $meal in // Add case for no meal
                             VStack(alignment: .leading) {
-                                Text(meal.name)
-                                    .font(.title)
-                                    .bold()
-                                HStack {
-                                    Image(systemName: findAttribute(status: meal.status, find: "SF"))
-                                    Text(meal.status.rawValue)
-                                }
-                                .foregroundColor(Color(findAttribute(status: meal.status, find: "Colour")))
-                                .font(.callout)
-                                HStack {
-                                    Image(systemName:"tray.and.arrow.down")
-                                    Text(daysBetween(start: Date.now, end: meal.storedDate))
-                                }
-                                .foregroundStyle(.gray)
-                                .font(.callout)
-                            }
-                            .padding(.horizontal, 15)
-                            HStack(alignment: .center, spacing: 20) {
-                                Spacer()
-                                VStack(alignment: .center) {
-                                    Text("Expiry")
-                                        .foregroundStyle(.secondary)
-                                        .font(.caption)
-                                    Text(daysBetween(start: Date.now, end: meal.expiryDate))
+                                VStack(alignment: .leading) {
+                                    Text(meal.name)
+                                        .font(.title)
                                         .bold()
-                                }
-                                .scaledToFill()
-                                .frame(maxWidth: 80)
-                                Divider()
-                                    .scaledToFill()
-                                    .frame(maxWidth: 2, maxHeight: 60)
-                                    .background(Color("HighlightPurple"))
-                                    .padding(.horizontal, 12)
-                                VStack(alignment: .center) {
-                                    Text("Planned for")
-                                        .foregroundStyle(.secondary)
-                                        .font(.caption)
-                                    Text(daysBetween(start: Date.now, end: meal.plannedDate))
-                                        .bold()
-                                }
-                                .scaledToFill()
-                                .frame(maxWidth: 80)
-                                Spacer()
-                            }
-                            .multilineTextAlignment(.center)
-                            HStack {
-                                Button {
-                                    meal.isConsumed.toggle()
-                                } label: {
-                                    HStack(alignment: .center) {
-                                        VStack(alignment: .center, spacing: 2) {
-                                            Text("Mark as")
-                                                .foregroundStyle(.secondary)
-                                                .font(.subheadline)
-                                            Text("Consumed")
-                                                .fontWeight(.heavy)
-                                                .foregroundStyle(.primary)
-                                                .font(.headline)
-                                        }
-                                        Image(systemName: meal.isConsumed ? "checkmark.square" : "square")
+                                    HStack {
+                                        Image(systemName: findAttribute(status: meal.status, find: "SF"))
+                                        Text(meal.status.rawValue)
                                     }
-                                    .padding(15)
-                                    .foregroundStyle(.white)
-                                    .frame(height: 50)
-                                    .scaledToFit()
-                                    .frame(maxWidth: .infinity)
-                                    .background(.accent)
-                                    .cornerRadius(10)
-                                    .animation(.bouncy, value: meal.isConsumed)
-                                    .shadow(radius: 5, y: 3)
+                                    .foregroundColor(Color(findAttribute(status: meal.status, find: "Colour")))
+                                    .font(.callout)
+                                    HStack {
+                                        Image(systemName:"tray.and.arrow.down")
+                                        Text(daysBetween(start: Date.now, end: meal.storedDate))
+                                    }
+                                    .foregroundStyle(.gray)
+                                    .font(.callout)
+                                }
+                                .padding(.horizontal, 15)
+                                HStack(alignment: .center, spacing: 20) {
+                                    Spacer()
+                                    VStack(alignment: .center) {
+                                        Text("Expiry")
+                                            .foregroundStyle(.secondary)
+                                            .font(.caption)
+                                        Text(daysBetween(start: Date.now, end: meal.expiryDate))
+                                            .bold()
+                                    }
+                                    .scaledToFill()
+                                    .frame(maxWidth: 80)
+                                    Divider()
+                                        .scaledToFill()
+                                        .frame(maxWidth: 2, maxHeight: 60)
+                                        .background(Color("HighlightPurple"))
+                                        .padding(.horizontal, 12)
+                                    VStack(alignment: .center) {
+                                        Text("Planned for")
+                                            .foregroundStyle(.secondary)
+                                            .font(.caption)
+                                        Text(daysBetween(start: Date.now, end: meal.plannedDate))
+                                            .bold()
+                                    }
+                                    .scaledToFill()
+                                    .frame(maxWidth: 80)
+                                    Spacer()
+                                }
+                                .multilineTextAlignment(.center)
+                                HStack {
+                                    Button {
+                                        mealManager.meals[mealManager.meals.firstIndex(of: meal)!].isConsumed = true
+                                        refreshUpcoming()
+                                    } label: {
+                                        HStack(alignment: .center) {
+                                            VStack(alignment: .center, spacing: 2) {
+                                                Text("Mark as")
+                                                    .foregroundStyle(.secondary)
+                                                    .font(.subheadline)
+                                                Text("Consumed")
+                                                    .fontWeight(.heavy)
+                                                    .foregroundStyle(.primary)
+                                                    .font(.headline)
+                                            }
+                                            Image(systemName: meal.isConsumed ? "checkmark.square" : "square")
+                                        }
+                                        .padding(15)
+                                        .foregroundStyle(.white)
+                                        .frame(height: 50)
+                                        .scaledToFit()
+                                        .frame(maxWidth: .infinity)
+                                        .background(.accent)
+                                        .cornerRadius(10)
+                                        .shadow(radius: 5, y: 3)
+                                    }
+                                    .padding(.horizontal, 10)
+                                    NavigationLink {
+                                        MealDetailView(mealManager: mealManager, meal: $meal)
+                                    } label: {
+                                        HStack(alignment: .center) {
+                                            VStack(alignment: .center, spacing: 2) {
+                                                Text("More")
+                                                    .foregroundStyle(.secondary)
+                                                    .font(.subheadline)
+                                                Text("Details")
+                                                    .fontWeight(.heavy)
+                                                    .foregroundStyle(.primary)
+                                                    .font(.headline)
+                                            }
+                                            Image(systemName: "chevron.right")
+                                        }
+                                        .padding(15)
+                                        .foregroundStyle(.white)
+                                        .frame(height: 50)
+                                        .scaledToFit()
+                                        .background(.accent)
+                                        .cornerRadius(10)
+                                        .shadow(radius: 5, y: 3)
+                                    }
+                                    .padding(.trailing, 10)
                                 }
                                 .padding(.horizontal, 10)
-                                NavigationLink {
-                                    MealDetailView(mealManager: mealManager, meal: $meal)
-                                } label: {
-                                    HStack(alignment: .center) {
-                                        VStack(alignment: .center, spacing: 2) {
-                                            Text("More")
-                                                .foregroundStyle(.secondary)
-                                                .font(.subheadline)
-                                            Text("Details")
-                                                .fontWeight(.heavy)
-                                                .foregroundStyle(.primary)
-                                                .font(.headline)
-                                        }
-                                        Image(systemName: "chevron.right")
-                                    }
-                                    .padding(15)
-                                    .foregroundStyle(.white)
-                                    .frame(height: 50)
-                                    .scaledToFit()
-                                    .background(.accent)
-                                    .cornerRadius(10)
-                                    .shadow(radius: 5, y: 3)
-                                }
-                                .padding(.trailing, 10)
                             }
-                            .padding(.horizontal, 10)
                         }
+                        .padding(.vertical, 30)
+                        .padding(.horizontal, 20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 30)
+                                .stroke(.accent, lineWidth: 3)
+                                .padding(3)
+                        )
+                    } else if upcomingMeals.isEmpty {
+                        Text("No upcoming meals")
+                            .foregroundStyle(.secondary)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .stroke(.accent, lineWidth: 3)
+                                    .padding(3)
+                                    .frame(width: 366, height: 260)
+                            )
                     }
-                    .padding(.vertical, 30)
-                    .padding(.horizontal, 20)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(.accent, lineWidth: 3)
-                            .padding(3)
-                    )
                 }
                 .padding(.bottom, -19)
                 .offset(y: -15)
-
+                
                 VStack(alignment: .leading) {
                     Group {
                         Text("Two ")
@@ -192,17 +203,9 @@ struct HomeView: View {
             .tabViewStyle(.page)
             .indexViewStyle(.page(backgroundDisplayMode: .always))
             .navigationTitle(welcomeMsg)
-            .toolbar {
-                Button {
-                    // refresh functionality
-                } label: {
-                    Image(systemName: "arrow.clockwise")
-                }
-            }
         }
         .onAppear {
             refreshUpcoming()
         }
-        .onChange(of: mealManager.meals) { _ in refreshUpcoming()}
     }
 }
